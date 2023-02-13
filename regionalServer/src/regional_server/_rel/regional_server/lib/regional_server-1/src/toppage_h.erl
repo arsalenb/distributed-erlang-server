b30b8cc158fd04ed2603843ca245261314b93635
brunocasu <brunocasu8@gmail.com>
2023-02-13 05:28:40 +0100

    basic erlang server running - POST response ok - JSON reponse ok

--- regionalServer/src/regional_server/_rel/regional_server/lib/regional_server-1/src/toppage_h.erl
+++ regionalServer/src/regional_server/_rel/regional_server/lib/regional_server-1/src/toppage_h.erl
@@ -3,6 +3,20 @@
 %% @doc POST echo handler.
 -module(toppage_h).
 
+-include_lib("eunit/include/eunit.hrl").
+
+-ifdef('NO_MAP_TYPE').
+-define(OBJ0, {[]}).
+-define(OBJ1(K, V), {[{K, V}]}).
+-define(OBJ2(K1, V1, K2, V2), {[{K1, V1}, {K2, V2}]}).
+-define(OBJECT_FROM_LIST(List), List).
+-else.
+-define(OBJ0, #{}).
+-define(OBJ1(K, V), #{K => V}).
+-define(OBJ2(K1, V1, K2, V2), #{K1 => V1, K2 => V2}).
+-define(OBJECT_FROM_LIST(List), maps:from_list(List)).
+-endif.
+
 -export([init/2]).
 
 init(Req0, Opts) ->
@@ -13,11 +27,19 @@ init(Req0, Opts) ->
 
 maybe_echo(<<"POST">>, true, Req0) ->
 	{ok, PostVals, Req} = cowboy_req:read_urlencoded_body(Req0),
-	%ID = proplists:get_value(<<"sensor_id">>, PostVals),
-	%Data = proplists:get_value(<<"sensor_data">>, PostVals),
-	Timestamp = proplists:get_value(<<"timestamp">>, PostVals),
-	%Echo = {ID, Data, Timestamp},
-	echo(Timestamp, Req);
+	ID = proplists:get_value(<<"sensor_id">>, PostVals),
+	Data = proplists:get_value(<<"sensor_data">>, PostVals),
+	Timestamp = proplists:get_value(<<"timestamp">>, PostVals), % PostVals are in BINARY format
+	JsonStart = <<"{\"sensor_id\":\"">>,
+	JsonData = <<"\", \"sensor_data\":\"">>,
+	JsonTimestamp = <<"\", \"timestamp\":\"">>,
+	JsonEnd = <<"\"}">>,
+	Body = <<	JsonStart/binary, ID/binary,
+						JsonData/binary, Data/binary,
+						JsonTimestamp/binary, Timestamp/binary,
+						JsonEnd/binary>>,
+	echo(Body, Req);
+
 maybe_echo(<<"POST">>, false, Req) ->
 	cowboy_req:reply(400, [], <<"Missing body.">>, Req);
 maybe_echo(_, _, Req) ->
--- regionalServer/src/regional_server/sensor_node_test.py
+++ regionalServer/src/regional_server/sensor_node_test.py
@@ -15,11 +15,17 @@ print("Data ", random_data)
 timestamp = datetime.now()
 print("Date and Time: ", timestamp)
 
-string_to_send = 'timestamp=' + str(timestamp)
+## toppage_h.erl -> erlang proplist:get_value function is used to get data from the POST message
+## toppage_h.erl -> the list is retrieved from cowboy request handler function cowboy_req:read_urlencoded_body
+## cowboy_req.erl -> cowboy request handler reads the binary values from the message using cow_qs:parse_qs(Body)
+## cow_qs.erl -> the parse function reads the characters from the binary form and copies to an erlang list
+## the parse function uses the '&' to seaparate key value pairs in the string
+## the data string format to be sent in the POST request is then: key1=value1&key2=value2&key3=value3...
+string_to_send = 'sensor_id='+str(random_id)+'&sensor_data='+str(random_data)+'&timestamp=' + str(timestamp) 
 print(string_to_send, '\n sending POST to server:')
 
 r = requests.post(SERVER_URL, data = string_to_send)
-print(r.text)
+print('Server Reply: ', r.text)
 #print(f"Status Code: {r.status_code}, Response: {r.json()}")
 
 
